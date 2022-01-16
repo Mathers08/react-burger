d48cabdb391fee713ab7f62dc2d3f09ee39e25d0
mathers08 <black.mathers08@mail.ru>
2022-01-16 14:33:41 +0300

    add favorite buttons

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar, faStarHalfAlt} from "@fortawesome/free-solid-svg-icons";
 import './Menu.scss';
 import titleImage from "../../assets/images/title-img.png";
-import prod1 from "../../assets/images/product-1.png";
-import prod2 from "../../assets/images/product-2.png";
-import prod3 from "../../assets/images/product-3.png";
-import prod4 from "../../assets/images/product-4.png";
-import prod5 from "../../assets/images/product-5.png";
-import prod6 from "../../assets/images/product-6.png";
+import axios from "axios";
 
 const Menu = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchedItems = async () => {
+      const {data} = await axios.get('http://localhost:3000/dbMenu.json');
+      setItems(data);
+    }
+    fetchedItems();
+  });
+
   return (
     <section className='menu' id='menu'>
       <div className="heading">
@@ -18,96 +23,26 @@ const Menu = () => {
         <h3>Our menu</h3>
       </div>
       <div className="box-container">
-        <div className="box" data-aos='fade-up' data-aos-delay='150'>
-          <img src={prod1} alt=""/>
-          <div className="content">
-            <div className="stars">
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
+        {items.map(item => (
+          <div key={item.id} className="box" data-aos='fade-up' data-aos-delay={item.dataAosDelay}>
+            <img src={item.imageUrl} alt=""/>
+            <div className="content">
+              <div className="stars">
+                <FontAwesomeIcon className='icons' icon={faStar}/>
+                <FontAwesomeIcon className='icons' icon={faStar}/>
+                <FontAwesomeIcon className='icons' icon={faStar}/>
+                <FontAwesomeIcon className='icons' icon={faStar}/>
+                <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
+              </div>
+              <h3>{item.title}</h3>
+              <div className="price">{item.price}</div>
+              <div className='btnBlock'>
+                <a href="#" className='btn'>Add to favorites</a>
+                <a href="#" className='btn'>Add to cart</a>
+              </div>
             </div>
-            <h3>Cheese hamburger</h3>
-            <div className="price">$29.99</div>
-            <a href="#" className='btn'>Add to cart</a>
           </div>
-        </div>
-        <div className="box" data-aos='fade-up' data-aos-delay='300'>
-          <img src={prod2} alt=""/>
-          <div className="content">
-            <div className="stars">
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
-            </div>
-            <h3>Cheese hamburger</h3>
-            <div className="price">$29.99</div>
-            <a href="#" className='btn'>Add to cart</a>
-          </div>
-        </div>
-        <div className="box" data-aos='fade-up' data-aos-delay='450'>
-          <img src={prod3} alt=""/>
-          <div className="content">
-            <div className="stars">
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
-            </div>
-            <h3>Cheese hamburger</h3>
-            <div className="price">$29.99</div>
-            <a href="#" className='btn'>Add to cart</a>
-          </div>
-        </div>
-        <div className="box" data-aos='fade-up' data-aos-delay='600'>
-          <img src={prod4} alt=""/>
-          <div className="content">
-            <div className="stars">
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
-            </div>
-            <h3>Cheese hamburger</h3>
-            <div className="price">$29.99</div>
-            <a href="#" className='btn'>Add to cart</a>
-          </div>
-        </div>
-        <div className="box" data-aos='fade-up' data-aos-delay='750'>
-          <img src={prod5} alt=""/>
-          <div className="content">
-            <div className="stars">
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
-            </div>
-            <h3>Cheese hamburger</h3>
-            <div className="price">$29.99</div>
-            <a href="#" className='btn'>Add to cart</a>
-          </div>
-        </div>
-        <div className="box" data-aos='fade-up' data-aos-delay='900'>
-          <img src={prod6} alt=""/>
-          <div className="content">
-            <div className="stars">
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStar}/>
-              <FontAwesomeIcon className='icons' icon={faStarHalfAlt}/>
-            </div>
-            <h3>Cheese hamburger</h3>
-            <div className="price">$29.99</div>
-            <a href="#" className='btn'>Add to cart</a>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
